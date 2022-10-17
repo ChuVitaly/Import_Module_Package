e36015066c6475ec3377013dc9cfb7a07e8e2170
Виталий Чухляев <vitaliy69@mail.ru>
2022-10-17 16:19:54 +0300

    Написал функции для этих модулей

---
+++ application/db/people.py
@@ -0,0 +1,2 @@
+def get_employees(name_firm, number_employees):
+    print(f"Firm: {name_firm} the number of employees: {number_employees}")
---
+++ application/salary.py
@@ -0,0 +1,2 @@
+def calculate_salary(name, salary):
+    print(f"Salary {name} is <=> {salary} $")
