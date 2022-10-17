912bd7e9a111c721d52884f9398440494ef53411
Виталий Чухляев <vitaliy69@mail.ru>
2022-10-17 16:21:57 +0300

    Запустил модули

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 
+from application import salary
+from application.db import people
+from datetime import datetime
+import matplotlib as ml
+
+ver_matlib = ml._get_version()
+
+
+# Запись версии библиотеки Matplotlib
+# with open("requirements.txt", "a") as f:
+#   f.write(f"Версия Matplotlib: {ver_matlib}")
 
 
 
 
 
+
+
+person_salary = salary.calculate_salary
+persons_in_firm = people.get_employees
+today_time = datetime.now()
 
 
 
 
 if __name__ == '__main__':
-  pass
+  person_salary("Mike", "10000")
+  persons_in_firm("Coca-cola", "2000")
+  print(today_time)
